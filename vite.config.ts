0a8eff62aecece3d766a3d7a92a7680a296c7149
Eric Wang <eric.wang2004nz@gmail.com>
2026-04-28 16:44:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Ricwan4.github/", 
+  base: "/Eric-rw-Wang-Web/", 
   plugins: [checker({ typescript: true })],
   worker: {},
   build: {
